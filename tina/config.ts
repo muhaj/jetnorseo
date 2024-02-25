7135db17b0d159fa06472eb8cf5e5dbc6116a650
Jetnor Muhaj <jmuhaj@gmail.com>
2024-02-25 19:04:42 +0000

    update

--- tina/config.ts
+++ tina/config.ts
@@ -41,6 +41,12 @@ export default defineConfig({
             required: true,
           },
           {
+            type: "datetime",
+            name: "posted",
+            label: "Date Posted",
+            required: true,
+          },
+          {
             type: "rich-text",
             name: "body",
             label: "Body",
